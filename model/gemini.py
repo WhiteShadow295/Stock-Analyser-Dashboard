eceba769e7bea9a51d679ec3257c900971a743a3
Wong Wei Jie <110820442+WhiteShadow295@users.noreply.github.com>
2024-11-07 12:07:57 +0000

    added a return type

--- model/gemini.py
+++ model/gemini.py
@@ -8,7 +8,7 @@ class geminiService:
         load_dotenv()
         self.api = os.getenv("GENAI_API_KEY")
     
-    def get_introduction(self, symbol: str, genaiModel="gemini-1.5-flash"):
+    def get_introduction(self, symbol: str, genaiModel="gemini-1.5-flash") -> str:
         # Get introduction of the company
         genai.configure(api_key=self.api)
         model = genai.GenerativeModel(genaiModel)
