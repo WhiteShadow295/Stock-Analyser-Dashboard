66f0162661ca42d7a1a77a8f3ba06652e488dbe7
Wong Wei Jie <110820442+WhiteShadow295@users.noreply.github.com>
2024-11-07 06:27:03 +0000

    added get historical function and move load_dotenv into init function

--- model/fmp.py
+++ model/fmp.py
@@ -2,12 +2,10 @@ import requests
 from dotenv import load_dotenv
 import os
 
-##  Setting up the env
-load_dotenv()
-
 class Fmp:
     
     def __init__(self):
+        load_dotenv()
         self.base_url = os.getenv("FMP_BASE_URL")
         self.api = os.getenv("FMP_API_KEY")
         
@@ -15,4 +13,20 @@ class Fmp:
         
         url = f'{self.base_url}/key-metrics/{symbol}?period={period}&apikey={self.api}'
         response = requests.get(url)
-        return response.json()
+        
+        if response.status_code != 200:
+            return {'error': 'An error occurred, check your ticker symbol',
+                    'errorcode': response.status_code}
+        else:
+            return response.json()
+        
+    def get_historical_data(self, symbol: str):
+        
+        url = f'{self.base_url}/historical-price-full/{symbol}?apikey={self.api}'
+        response = requests.get(url)
+        
+        if response.status_code != 200:
+            return {'error': 'An error occurred, check your ticker symbol',
+                    'errorcode': response.status_code}
+        else:
+            return response.json()
